1442c4eb0f849cf393b66f0dfa7946bb927a488a
Bohdan Strilets <bohdan.strilets@gmail.com>
2023-12-04 14:10:13 +0100

    Added method for send repeat activation mail.

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Get, Redirect, Param } from '@nestjs/common';
+import { Controller, Get, Redirect, Param, Body, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { ResponseType } from './types/response.type';
 import { CLIENT_URL } from 'src/utilities/constants';
+import { EmailDto } from './dto/email.dto';
 
 @Controller('users')
 export class UsersController {
@@ -13,6 +14,13 @@ export class UsersController {
     @Param('activationToken') activationToken: string,
   ): Promise<ResponseType | undefined> {
     const data = await this.usersService.activationEmail(activationToken);
+    return data;
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Post('repeat-activation-email')
+  async repeatActivationEmail(@Body() emailDto: EmailDto): Promise<ResponseType | undefined> {
+    const data = await this.usersService.repeatActivationEmail(emailDto);
     return data;
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,12 +1,14 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
+import { v4 } from 'uuid';
 import { User, UserDocument } from './schemas/user.schema';
 import { Token, TokenDocument } from 'src/tokens/schemas/token.schema.ts';
 import { SendgridService } from 'src/sendgrid/sendgrid.service';
 import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
 import { TokensService } from 'src/tokens/tokens.service';
 import { ResponseType } from './types/response.type';
+import { EmailDto } from './dto/email.dto';
 
 @Injectable()
 export class UsersService {
@@ -40,6 +42,34 @@ export class UsersService {
       code: HttpStatus.OK,
       success: true,
       message: 'Email is successfully activated.',
+    };
+  }
+
+  async repeatActivationEmail(emailDto: EmailDto): Promise<ResponseType | undefined> {
+    const user = await this.UserModel.findOne({ email: emailDto.email });
+
+    if (!user) {
+      throw new HttpException(
+        {
+          status: 'error',
+          code: HttpStatus.NOT_FOUND,
+          success: false,
+          message: 'User not found.',
+        },
+        HttpStatus.NOT_FOUND,
+      );
+    }
+
+    const activationToken = v4();
+    const mail = this.sendgridService.confirmEmail(user.email, activationToken);
+    await this.sendgridService.sendEmail(mail);
+    await this.UserModel.findByIdAndUpdate(user._id, { isActivated: false, activationToken });
+
+    return {
+      status: 'success',
+      code: HttpStatus.OK,
+      success: true,
+      message: 'The confirmation email has been sent again.',
     };
   }
 }
