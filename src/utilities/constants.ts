819a19285d7b6f7f75f2b1de35dfa4775c10316f
Bohdan Strilets <bohdan.strilets@gmail.com>
2023-12-09 11:12:44 +0100

    The file with variables has been cleaned.

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -5,4 +5,3 @@ export const CLIENT_URL = 'http://localhost:3000/work-time';
 export const CORS_CLIENT_URL = 'http://localhost:3000';
 export const AMOUNT_SALT = 10;
 export const REFRESH_TOKEN = 'refresh-token';
-export const CALLBACK_URL = 'http://localhost:5050/api/v1/auth/google/callback';
