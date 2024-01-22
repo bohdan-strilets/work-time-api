587fdb060aa25c0648da7da036e18ec79cf2ce53
Bohdan Strilets <87972200+bohdan-strilets@users.noreply.github.com>
2024-01-22 21:33:20 +0100

    Update constants.ts

--- src/utilities/constants.ts
+++ src/utilities/constants.ts
@@ -1,6 +1,7 @@
 export const MIN_PASSWORD_LENGTH = 6;
 export const MAX_PASSWORD_LENGTH = 12;
 export const API_URL = 'https://work-time-api.vercel.app/';
-export const CLIENT_URL = 'http://localhost:3000';
+// export const CLIENT_URL = 'http://localhost:3000';
+export const CLIENT_URL = 'https://work-time-1-0-0.netlify.app/';
 export const AMOUNT_SALT = 10;
 export const REFRESH_TOKEN = 'refresh-token';
