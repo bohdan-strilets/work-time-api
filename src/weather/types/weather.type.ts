d2b4238a10b2ff34a7dfbc407c8d730b5f16b5ac
Bohdan Strilets <bohdan.strilets@gmail.com>
2024-02-15 20:37:47 +0100

    Changed weather type.

--- src/weather/types/weather.type.ts
+++ src/weather/types/weather.type.ts
@@ -1,7 +1,18 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class WeatherType {
+  @ApiProperty()
   averageTemperature: number;
+
+  @ApiProperty()
   weatherCondition: string;
+
+  @ApiProperty()
   weatherIcon: string;
+
+  @ApiProperty()
   maximumTemperature: number;
+
+  @ApiProperty()
   minimumTemperature: number;
 }
