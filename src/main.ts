903d503a536a020b9b1588d3583e457f89ef55d4
Bohdan Strilets <bohdan.strilets@gmail.com>
2024-02-18 19:18:59 +0100

    Fixed swagger,

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,13 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/docs', app, document);
+  SwaggerModule.setup('/docs', app, document, {
+    customCssUrl: 'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
+    customJs: [
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.js',
+      'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.js',
+    ],
+  });
 
   app.setGlobalPrefix('api/v1');
   app.use(cookieParser());
